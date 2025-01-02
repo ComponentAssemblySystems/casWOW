823a926ef3c8a7abd16fd4b75f3812060af67704
AdamJ <adam.jolicoeur@gmail.com>
2025-01-02 15:40:36 -0500

    Update base paths and fix missing icon fonts

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
+  env: process.env.ELEVENTY_ENV,
   url: process.env.URL || "http://localhost:8080",
-  siteDescription: "The portfolio site for Adam Jolicoeur.",
   authorName: "Adam J. Jolicoeur",
   name: "casWOW"
 }
