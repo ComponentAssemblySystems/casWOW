a6f1e3af61e52724da8753ce19a00c8acdb669b0
AdamJ <adam.jolicoeur@gmail.com>
2025-10-10 13:55:32 -0400

    storybook: remove header ref from page

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,5 +1,3 @@
-import { expect, userEvent, within } from 'storybook/test';
-
 import { createPage } from './Page';
 
 export default {
@@ -12,17 +10,3 @@ export default {
 };
 
 export const LoggedOut = {};
-
-// More on component testing: https://storybook.js.org/docs/writing-tests/interaction-testing
-export const LoggedIn = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = canvas.getByRole('button', { name: /Log in/i });
-    await expect(loginButton).toBeInTheDocument();
-    await userEvent.click(loginButton);
-    await expect(loginButton).not.toBeInTheDocument();
-
-    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
-    await expect(logoutButton).toBeInTheDocument();
-  },
-};
