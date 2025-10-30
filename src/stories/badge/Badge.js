b07410b5d67ac56863d4c42965cbd5b3ac5b00d2
AdamJ <adam.jolicoeur@gmail.com>
2025-10-30 10:48:16 -0400

    badge: add outline variant

--- src/stories/badge/Badge.js
+++ src/stories/badge/Badge.js
@@ -1,32 +1,54 @@
 export const createBadge = ({
   primary = false,
+  outline = false,
   count = false,
   label
 }) => {
   const badge = document.createElement('span');
 
   let mode;
-  if (count) {
-    mode = 'position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger';
-    // if (primary) {
-    //   mode = 'position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger';
-    // } else if (label === 'Secondary Pill') {
-    //   mode = 'rounded-pill text-bg-secondary';
-    // } else if (label === 'Success Pill') {
-    //   mode = 'rounded-pill text-bg-success';
-    // } else if (label === 'Danger Pill') {
-    //   mode = 'rounded-pill text-bg-danger';
-    // } else if (label === 'Warning Pill') {
-    //   mode = 'rounded-pill text-bg-warning';
-    // } else if (label === 'Info Pill') {
-    //   mode = 'rounded-pill text-bg-info';
-    // } else if (label === 'Light Pill') {
-    //   mode = 'rounded-pill text-bg-light';
-    // } else if (label === 'Dark Pill') {
-    //   mode = 'rounded-pill text-bg-dark';
-    // }
+  // if (count) {
+  //   mode = 'position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger';
+  //   // if (primary) {
+  //   //   mode = 'position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger';
+  //   // } else if (label === 'Secondary Pill') {
+  //   //   mode = 'rounded-pill text-bg-secondary';
+  //   // } else if (label === 'Success Pill') {
+  //   //   mode = 'rounded-pill text-bg-success';
+  //   // } else if (label === 'Danger Pill') {
+  //   //   mode = 'rounded-pill text-bg-danger';
+  //   // } else if (label === 'Warning Pill') {
+  //   //   mode = 'rounded-pill text-bg-warning';
+  //   // } else if (label === 'Info Pill') {
+  //   //   mode = 'rounded-pill text-bg-info';
+  //   // } else if (label === 'Light Pill') {
+  //   //   mode = 'rounded-pill text-bg-light';
+  //   // } else if (label === 'Dark Pill') {
+  //   //   mode = 'rounded-pill text-bg-dark';
+  //   // }
+  // } else {
+  if (outline) {
+    if (primary) {
+      mode = 'text-bg-primary bg-transparent border border-primary text-primary';
+    } else if (label === 'Secondary') {
+      mode = 'text-bg-secondary bg-transparent border border-secondary text-secondary';
+    } else if (label === 'Success') {
+      mode = 'text-bg-success bg-transparent border border-success text-success';
+    } else if (label === 'Danger') {
+      mode = 'text-bg-danger bg-transparent border border-danger text-danger';
+    } else if (label === 'Warning') {
+      mode = 'text-bg-warning bg-transparent border border-warning text-warning';
+    } else if (label === 'Info') {
+      mode = 'text-bg-info bg-transparent border border-info text-info';
+    } else if (label === 'Light') {
+      mode = 'text-bg-light bg-transparent border border-light-subtle text-black-50';
+    } else if (label === 'Dark') {
+      mode = 'text-bg-dark bg-transparent border border-dark-subtle text-dark';
+    } else {
+      mode = 'text-bg-secondary bg-transparent border border-secondary text-secondary';
+    }
   } else {
-  if (primary) {
+    if (primary) {
       mode = 'text-bg-primary';
     } else if (label === 'Secondary') {
       mode = 'text-bg-secondary';
@@ -43,7 +65,7 @@ export const createBadge = ({
     } else if (label === 'Dark') {
       mode = 'text-bg-dark';
     } else {
-      mode = 'text-bg-primary';
+      mode = 'text-bg-secondary';
     }
   }
 
