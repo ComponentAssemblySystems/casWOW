7db840371e7df0667d3c4963195ef39981f2aa05
AdamJ <adam.jolicoeur@gmail.com>
2025-10-30 17:44:41 -0400

    major: remove storybook

--- src/stories/ListItem.js
+++
@@ -1,8 +0,0 @@
-export const createListItem = ({ text = '', selected = false }) => {
-  const item = document.createElement('li');
-  item.innerText = text;
-  if (selected) {
-    item.className = 'selected';
-  }
-  return item;
-};
