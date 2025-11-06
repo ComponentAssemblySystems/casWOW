2848aa3d812fe4e366272c40187712e1f296ecbc
AdamJ <adam.jolicoeur@gmail.com>
2025-11-06 14:25:45 -0500

    feat: new project cost pages

--- src/_data/jobCashflow.js
+++ src/_data/jobCashflow.js
@@ -2,26 +2,36 @@ module.exports = [
   {
     widgetTitle: "Contract total",
     widgetPercentage: "100%",
-    widgetNumber: "$517,425,000"
+    widgetNumber: "$517,425,000",
+    isNegative: false,
+    isWarning: false
   },
   {
     widgetTitle: "Total costs",
     widgetPercentage: "+75%",
-    widgetNumber: "$389,929,024"
+    widgetNumber: "$389,929,024",
+    isNegative: true,
+    isWarning: false
   },
   {
     widgetTitle: "Billed to date",
     widgetPercentage: "100%",
-    widgetNumber: "$390,512,672"
+    widgetNumber: "$390,512,672",
+    isNegative: false,
+    isWarning: false
   },
   {
     widgetTitle: "Payments received",
     widgetPercentage: "91%",
-    widgetNumber: "$355,410,415"
+    widgetNumber: "$355,410,415",
+    isNegative: false,
+    isWarning: false
   },
   {
     widgetTitle: "Unbilled",
     widgetPercentage: "7%",
-    widgetNumber: "$2,744,032"
+    widgetNumber: "$2,744,032",
+    isNegative: false,
+    isWarning: true
   }
 ];
