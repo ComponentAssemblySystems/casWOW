fd61e7f2c3493d1a7ce1fb2339ba6514f890c569
Adam Jolicoeur <ajolicoeur@componentassembly.com>
2026-09-18 16:13:02 -0400

    fix: broken cas- styles

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -3,5 +3,8 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   authorName: "Adam J. Jolicoeur",
   name: "casWOW",
+  siteName: "casWOW",
+  siteDescription: "Construction project management dashboard for CAS",
+  siteMeta: "construction, project management, dashboard, cas",
   version: "6.0.4.24"
 }
